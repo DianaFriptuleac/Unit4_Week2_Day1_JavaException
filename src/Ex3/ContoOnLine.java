3b2df8802eb5ffa4eb93e65195e07df0f14a41cd
Diana <dianadorojuc3@gmail.com>
2024-09-30 16:07:04 +0200

    ex2

---
+++ src/Ex3/ContoOnLine.java
@@ -0,0 +1,21 @@
+package Ex3;
+
+public class ContoOnLine extends ContoCorrente {
+    private double maxPrelievo;
+
+    public ContoOnLine(String titolare, double saldo, double maxP) {
+        super(titolare, saldo);
+        this.maxPrelievo = maxP;
+    }
+
+    public void stampaSaldo() {
+        System.out.println("Titolare: " + titolare + " - Saldo: " + getSaldo() + " - Num movimenti: " + getnMovimenti()
+                + " - Massimo movimenti: " + getMaxMovimenti() + " - Massimo prelievo possibile: " + maxPrelievo);
+    }
+
+    public void preleva(double x) {
+        if (x <= maxPrelievo) {
+            super.preleva(x);
+        }
+    }
+}
