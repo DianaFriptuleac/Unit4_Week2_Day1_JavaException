fd2e5075397e80d360796ba2684a1974c539837a
Diana <dianadorojuc3@gmail.com>
2024-09-30 16:44:16 +0200

    ex3

--- src/Ex3/Main3.java
+++ src/Ex3/Main3.java
@@ -4,28 +4,32 @@ import java.util.Scanner;
 
 public class Main3 {
     public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
 
         //Istanzio un ContoCorrente e un ContoOnline
+        ContoCorrente contoAnna = new ContoCorrente("Anna Margini", 3850);
+        ContoOnLine contoAnnaOnline = new ContoOnLine("Luigi Verdi", 4500, 600);
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            ContoCorrente contoAnna = new ContoCorrente("Anna Margini", 3850);
-            ContoOnLine contoAnnaOnline = new ContoOnLine("Luigi Verdi", 4500, 600);
+        try{
             //Prelievo ContoCorrente
             System.out.println("Inserisci l'importo da prelevare dal tuo ContoCorrente: ");
             double conntanti = scanner.nextDouble();
             contoAnna.preleva(conntanti);
-            System.out.println("Prelevato! Il saldo attuale: " + contoAnna.restituisciSaldo() + " euro");
+            System.out.println("Prelevato! Il saldo attuale: "+ contoAnna.restituisciSaldo() + " euro");
 
             //Prelievo ContoOnline
             System.out.println("Inserisci l'importo da prelevare dal tuo ContoOnline: ");
             double conntanti2 = scanner.nextDouble();
             contoAnnaOnline.preleva(conntanti2);
-            System.out.println("Prelevato! Il saldo attuale: " + contoAnnaOnline.restituisciSaldo() + " euro");
+            System.out.println("Prelevato! Il saldo attuale: "+ contoAnnaOnline.restituisciSaldo() + " euro");
 
-        } catch (BancaException e) {
-            System.out.println("Errore " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Errore generico " + e.getMessage());
+        } catch (BancaException e){
+            //se il prelievo non e valido
+            System.out.println("Errore " +e.getMessage());
+        } catch (Exception e){
+            System.out.println("Errore generico "+ e.getMessage());
+        }finally {
+            scanner.close();
         }
     }
 }
